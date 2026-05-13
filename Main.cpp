8fa236f6c842cbe711c030daec8e151e8367d02f
Ashna <ashnasamibutt@gmail.com>
2026-05-13 21:25:51 +0500

    Add Main.cpp and update README

---
+++ Main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class Box{
+ private:
+    double volume;
+public:
+    Box (){
+        cout<<"Default constructor is called"<<endl;
+    }
+    Box(double v):volume(v) {
+        cout<<"Parameterized constructor is called"<<endl;
+    }
+    bool operator == (const Box&other){
+        return volume == other.volume;
+    }
+};
+int main{
+    Box b1(50);
+    Box b2(50);
+    Box b3(30);
+    if(b1==b2){
+        cout<<"b1 and b2 are identical"<<endl;
+    }
+    else 
+    cout<<"b1 and b2 are not identical"<<endl;
+    if(b1==b3){
+        cout<<"b1 and b3 are identical"<<endl;
+    }
+    else 
+    cout<<"b1 and b3 are not identical"<<endl;
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Start-
+Hey there it's my first repository just tryna do smthg via terminal.
+Readme.md file is created. 
