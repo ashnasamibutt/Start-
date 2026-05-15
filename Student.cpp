f26f333aa3005765dae9478ec27e744ccc3b7b6b
Ashna <ashnasamibutt@gmail.com>
2026-05-15 17:53:33 +0500

    Add Student class with getters and setters

---
+++ Student.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Student{
+private:
+   string name;
+   string roll_No;
+   int age;
+   double marks;
+public:
+   Student() : name("Unknown"),roll_No("A123"),age(18),marks(0){
+    cout<<"Default constructor is called"<<endl;
+   }
+
+   Student(string n ,string r,int a,double m) : name(n),roll_No(r),age(a),marks(m) { 
+    cout<<"Parameterized constructor is called"<<endl;
+   }
+
+   void setname(string n){
+    name = n;
+   }
+   void setrollno(string r){
+    roll_No = r;
+   }
+   void setage(int a){
+    age = a;
+   }
+   void setmarks(double m){
+    marks = m;
+   }
+
+   string getName(){
+    return name;
+   }
+   string getRollNo(){
+    return roll_No;
+   }
+   int getAge(){
+    return age;
+   }
+   double getMarks(){
+    return marks;
+   }
+
+   void displayInfo(){
+    cout<<"Name: "<<name<<endl;
+    cout<<"RollNO: "<<roll_No<<endl;
+    cout<<"Age: "<<age<<endl;
+    cout<<"Marks: "<<marks<<endl;
+   }
+};
+
+int main (){
+    Student s1;
+    Student s2("Rida","CS-100",19,90.5);
+    s2.setmarks(100);
+    s1.displayInfo();
+    cout<<"-----------------------------"<<endl;
+    s2.displayInfo();
+}
